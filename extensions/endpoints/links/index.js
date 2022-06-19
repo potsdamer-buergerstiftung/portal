01ae28711511482497812360016769f7e7617299
Nöl <65312133+linlogge@users.noreply.github.com>
2022-06-19 02:08:06 +0200

    delete links ext

--- extensions/endpoints/links/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";var e=(e,{services:s,exceptions:c})=>{const{ItemsService:n}=s,{ServiceUnavailableException:t}=c;e.get("/",(async(e,s,c)=>{new n("links").readByQuery().then((e=>s.json(e))).catch((e=>c(new t(e.message))))}))};module.exports=e;
