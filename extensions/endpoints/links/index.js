0cecb494ccf1a804742a454eb042d2bb4e1bfd25
Nöl <65312133+linlogge@users.noreply.github.com>
2022-06-19 02:04:23 +0200

    add test for module

--- extensions/endpoints/links/index.js
+++ extensions/endpoints/links/index.js
@@ -1,1 +1,1 @@
-"use strict";var e=e=>{e.get("/",((e,l)=>l.send("Hello, World!")))};module.exports=e;
+"use strict";var e=(e,{services:s,exceptions:c})=>{const{ItemsService:n}=s,{ServiceUnavailableException:t}=c;e.get("/",(async(e,s,c)=>{new n("links").readByQuery().then((e=>s.json(e))).catch((e=>c(new t(e.message))))}))};module.exports=e;
--- packages/links/src/index.ts
+++ packages/links/src/index.ts
@@ -1,5 +1,16 @@
-import { defineEndpoint } from '@directus/extensions-sdk';
+import { defineEndpoint } from "@directus/extensions-sdk";
 
-export default defineEndpoint((router) => {
-	router.get('/', (_req, res) => res.send('Hello, World!'));
+export default defineEndpoint((router, { services, exceptions }) => {
+  const { ItemsService } = services;
+  const { ServiceUnavailableException } = exceptions;
+
+  router.get("/", async (_req, res, next) => {
+    const linksService = new ItemsService("links");
+    linksService
+      .readByQuery()
+      .then((results: any) => res.json(results))
+      .catch((error: any) => {
+        return next(new ServiceUnavailableException(error.message));
+      });
+  });
 });
